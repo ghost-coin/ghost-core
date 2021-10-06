34e0ee0b6e8ff37df817d60106f2da91bb7c6cb1
tecnovert <tecnovert@tecnovert.net>
2021-10-06 14:01:16 +0200

    depends: Revert to using autotools for hidapi

--- depends/packages/hidapi.mk
+++ depends/packages/hidapi.mk
@@ -5,12 +5,17 @@ $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=391d8e52f2d6a5cf76e2b0c079cfefe25497ba1d4659131297081fc0cd744632
 $(package)_linux_dependencies=libusb
 
-define $(package)_config_cmds
-  $($(package)_cmake) -DBUILD_SHARED_LIBS="OFF"
+define $(package)_set_vars
+  $(package)_config_opts=--disable-shared
+  $(package)_config_opts_linux=--with-pic
 endef
 
-define $(package)_build_cmds
-  $(MAKE)
+define $(package)_preprocess_cmds
+  cd $($(package)_build_subdir); ./bootstrap
+endef
+
+define $(package)_config_cmds
+ $($(package)_autoconf)
 endef
 
 define $(package)_stage_cmds
