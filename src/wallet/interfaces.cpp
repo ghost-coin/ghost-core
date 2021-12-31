cc812ea9a9612db7757442879d82724af2cc92d5
tecnovert <tecnovert@tecnovert.net>
2021-12-31 11:06:50 +0200

    qt: Fix immature staked txns showing stake kernel amount

--- src/wallet/interfaces.cpp
+++ src/wallet/interfaces.cpp
@@ -96,17 +96,25 @@ WalletTx MakeWalletTx(CWallet& wallet, const CWalletTx& wtx)
                 result.txout_address_is_mine.emplace_back(ISMINE_NO);
             }
         }
-    } else {
-        result.txout_is_mine.reserve(wtx.tx->vout.size());
-        result.txout_address.reserve(wtx.tx->vout.size());
-        result.txout_address_is_mine.reserve(wtx.tx->vout.size());
-        for (const auto& txout : wtx.tx->vout) {
-            result.txout_is_mine.emplace_back(wallet.IsMine(txout));
-            result.txout_address.emplace_back();
-            result.txout_address_is_mine.emplace_back(ExtractDestination(txout.scriptPubKey, result.txout_address.back()) ?
-                                                          wallet.IsMine(result.txout_address.back()) :
-                                                          ISMINE_NO);
-        }
+        result.credit = CachedTxGetCredit(wallet, wtx, ISMINE_ALL, true);
+        result.debit = CachedTxGetDebit(wallet, wtx, ISMINE_ALL);
+        result.change = CachedTxGetChange(wallet, wtx);
+        result.time = wtx.GetTxTime();
+        result.value_map = wtx.mapValue;
+        result.is_coinbase = wtx.IsCoinBase();
+        result.is_coinstake = wtx.IsCoinStake();
+        return result;
+    }
+
+    result.txout_is_mine.reserve(wtx.tx->vout.size());
+    result.txout_address.reserve(wtx.tx->vout.size());
+    result.txout_address_is_mine.reserve(wtx.tx->vout.size());
+    for (const auto& txout : wtx.tx->vout) {
+        result.txout_is_mine.emplace_back(wallet.IsMine(txout));
+        result.txout_address.emplace_back();
+        result.txout_address_is_mine.emplace_back(ExtractDestination(txout.scriptPubKey, result.txout_address.back()) ?
+                                                      wallet.IsMine(result.txout_address.back()) :
+                                                      ISMINE_NO);
     }
     result.credit = CachedTxGetCredit(wallet, wtx, ISMINE_ALL);
     result.debit = CachedTxGetDebit(wallet, wtx, ISMINE_ALL);
