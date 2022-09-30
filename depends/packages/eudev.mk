f40bbdf3ea51e86d722f1b9419e72fc8e364d32a
tecnovert <tecnovert@tecnovert.net>
2022-09-30 23:59:50 +0200

    depends: Update eudev to 3.2.11
    remove std=c11 option, typeof is a compiler extension.

--- depends/packages/eudev.mk
+++ depends/packages/eudev.mk
@@ -1,12 +1,13 @@
 package=eudev
-$(package)_version=3.2.10
-$(package)_download_path=https://github.com/eudev-project/eudev/archive/
-$(package)_file_name=v$($(package)_version).tar.gz
-$(package)_sha256_hash=6492629da4024d2d21bb1a79d724e013d4152956099a5c63b09c8ee4da7f9b2b
+$(package)_version=3.2.11
+$(package)_download_path=https://github.com/eudev-project/eudev/releases/download/v$($(package)_version)/
+$(package)_file_name=$(package)-$($(package)_version).tar.gz
+$(package)_sha256_hash=19847cafec67897da855fde56f9dc7d92e21c50e450aa79068a7e704ed44558b
 
 define $(package)_set_vars
   $(package)_config_opts=--disable-static --disable-manpages --disable-programs
   $(package)_config_opts_linux=--with-pic
+  $(package)_cflags=-pipe
 endef
 
 define $(package)_preprocess_cmds
