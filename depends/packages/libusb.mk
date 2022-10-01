eaa3182970c8b8582d6e637d0a7ec5a64303abc2
tecnovert <tecnovert@tecnovert.net>
2022-10-02 01:41:58 +0200

    depends: Raise libusb version to 1.0.26

--- depends/packages/libusb.mk
+++ depends/packages/libusb.mk
@@ -1,17 +1,13 @@
 package=libusb
-$(package)_version=1.0.24
-$(package)_download_path=https://github.com/libusb/libusb/archive/
-$(package)_file_name=v$($(package)_version).tar.gz
-$(package)_sha256_hash=b7724c272dfc5713dce88ff717efd60f021ca5b7c8e30f08ebb2c42d2eea08ae
+$(package)_version=1.0.26
+$(package)_download_path=https://github.com/libusb/libusb/releases/download/v$($(package)_version)
+$(package)_file_name=libusb-$($(package)_version).tar.bz2
+$(package)_sha256_hash=12ce7a61fc9854d1d2a1ffe095f7b5fac19ddba095c259e6067a46500381b5a5
 $(package)_linux_dependencies=eudev
 
 define $(package)_set_vars
   $(package)_config_opts=--disable-shared --enable-udev
   $(package)_config_opts_linux=--with-pic
-endef
-
-define $(package)_preprocess_cmds
-  cd $($(package)_build_subdir); ./bootstrap.sh
 endef
 
 define $(package)_config_cmds
