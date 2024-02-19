03469cd607a695fbc3979be195e42bdb0889565d
bleach86 <14421739+bleach86@users.noreply.github.com>
2024-02-19 09:35:28 -0600

    adjust min protocol

--- src/version.h
+++ src/version.h
@@ -16,7 +16,7 @@ static const int PROTOCOL_VERSION = 90050;
 static const int INIT_PROTO_VERSION = 209;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = PROTOCOL_VERSION;
+static const int MIN_PEER_PROTO_VERSION = 90040; //PROTOCOL_VERSION;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
